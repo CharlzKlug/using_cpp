957533c410cae1fbea792f283b45c647e774996c
Imil Ametov <charlz_klug@mail.ru>
2023-10-30 09:55:23 +0300

    Exercise 2 finished

--- 04/169_exercises/02/main.cpp
+++ 04/169_exercises/02/main.cpp
@@ -1,10 +1,11 @@
 #include <vector> // Для std::vector
+#include <iostream> // Для std::cout
 
 enum Parity {Odd, Even};
 
 Parity numberParity(const size_t x) {
-    if (x % 2 == 0) return Parity::Odd;
-    return Parity::Even;
+    if (x % 2 == 0) return Parity::Even;
+    return Parity::Odd;
 }
 
 double median(std::vector<double> inputVector) {
@@ -14,11 +15,22 @@ double median(std::vector<double> inputVector) {
 	size_t middleIndex= vectorLength / 2;
 	result= inputVector.at(middleIndex);
     } else {
-	
+	size_t half_index= (vectorLength / 2) - 1;
+	result= (inputVector.at(half_index) +
+		 inputVector.at(half_index + 1)) / 2;
     }
+    return result;
 }
 
 int main() {
     std::vector<double> myVector= {0, 1, 57,58,59};
+    std::cout << median(myVector) << std::endl;
+
+    std::vector<double> testVector= {3};
+    std::cout << median(testVector) << std::endl;
+
+    std::vector<double> testVector2= {0, 1, 2, 4};
+    std::cout << median(testVector2) << std::endl;
+    
     return 0;
 }
